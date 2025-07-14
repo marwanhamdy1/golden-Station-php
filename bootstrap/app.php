7dc6b9a7e99f7584529b14a06620c321bbe6cddb
marwanbemo <marwan@godrve.com>
2025-07-14 13:14:58 +0300

    fix cros

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth.agent' => \App\Http\Middleware\AuthenticateAgent::class,
         ]);
+        // Register global CORS middleware
+        $middleware->append([\Illuminate\Http\Middleware\HandleCors::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
